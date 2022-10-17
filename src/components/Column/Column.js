b794751d5270268c8a73b62938dc89869f2d39d4
Joanna Olszewska <j.olszewskaa@gmail.com>
2022-10-17 08:57:14 +0200

    Error addCard && addColumn not working

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -13,7 +13,7 @@ const Column = props => {
           <ul className={styles.cards}>
           {cards.map(card => <Card key={card.id} title={card.title} />)}
           </ul>
-          <CardForm columnId={props.id} />
+          <CardForm columnId={props.columnId} />
         </article>
     );
 };
