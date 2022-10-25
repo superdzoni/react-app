c776551834e223fb9d1db5e82871c257d54b8e0f
Joanna Olszewska <j.olszewskaa@gmail.com>
2022-10-25 16:06:25 +0200

    Add removeCard action

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import styles from './Card.module.scss';
 import clsx from 'clsx';
 import { useDispatch } from 'react-redux';
-import { toggleCardFavorite } from '../../redux/cardsRedux';
+import { toggleCardFavorite, removeCard } from '../../redux/cardsRedux';
 
 const Card = ({ id, title, isFavorite }) => {
 
@@ -10,11 +10,21 @@ const Card = ({ id, title, isFavorite }) => {
   const toggle = () => {
     dispatch(toggleCardFavorite(id, isFavorite));
   }
+
+  const remove = () => {
+    dispatch(removeCard(id));
+  }
+
 	return (
 	  <li className={styles.card}>{title}
-		  <button onClick={toggle} className={clsx(styles.star_button, isFavorite && styles.active)}>
+		  <div>
+      <button onClick={toggle} className={clsx(styles.star_button, isFavorite && styles.active)}>
 				<span className='fa fa-star-o' />
 			</button>
+      <button onClick={remove} className={clsx(styles.remove_button)} >
+        <span className='fa fa-trash' />
+      </button>
+      </div>
 		</li>
 	);
 };
